4a67be70cf36372a052dc4d44b0d9108c77db566
Yuti Thakkar <thakkaryuti@gmail.com>
2023-10-11 22:20:17 -0400

    rerouting files for results

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from '/src/components/App.jsx'
+//import App from './App.jsx'
 import '/src/index.css'
 
 const renderApp = async () => {
