99971223c289ac417e77fe7172a14ff4ad738959
Yuti Thakkar <thakkaryuti@gmail.com>
2023-10-14 21:06:45 -0400

    Updated Component code

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,29 +1,57 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 const SearchForm = () => {
   const [query, setQuery] = useState('');
-  const history = useHistory();
+  const [searchResults, setSearchResults] = useState([]);
+  const [recipes, setRecipes] = useState([]);
+  // Effect hook to fetch recipes when the component mounts
+  useEffect(() => {
+    // Fetching recipes from the API using Axios
+    axios
+      .get('http://localhost:5000/recipes')
+      .then((response) => setRecipes(response.data))
+      .catch((error) => console.error('Error fetching recipes:', error));
+  }, []);
+  const handleSearch = () => {
+    const results = recipes.filter((recipe) =>
+      recipe.title.toLowerCase().includes(query.toLowerCase())
+    );
+    setSearchResults(results);
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Redirect to search results
-    history.push(`/search?q=${encodeURIComponent(query)}`);
+    handleSearch();
   };
-
   return (
-    <div>
-      <h2>Search recipes </h2>
+    <>
+      <h2>Search recipes</h2>
       <form onSubmit={handleSubmit}>
         <input
+          name="search"
           type="text"
           placeholder="Search for recipes..."
           value={query}
           onChange={(e) => setQuery(e.target.value)}
         />
-        <button type="submit">Search</button>
+        <button type="submit">🔍</button>
       </form>
-    </div>
+      {searchResults.length > 0 ? (
+        <>
+          <h3>Search Results:</h3>
+          <ul>
+            {searchResults.map((recipe) => (
+              <li key={recipe.id}>
+                <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>No recipe found.</p>
+      )
+      }
+    </>
   );
 };
-
 export default SearchForm;
